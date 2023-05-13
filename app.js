afc7d5fd5834f60b5efba58060a688132eb027cc
Amit Bhoyar <89729194+bhoyaramit@users.noreply.github.com>
2023-05-13 13:03:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import express from "express";
-const app = express();
 import {config} from "dotenv";
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/error.js";
@@ -7,17 +6,19 @@ import userRoute from "./routes/userRoute.js";
 import taskRoute from "./routes/taskRoute.js";
 import cors from "cors";
 
+export const app = express();
+
+
 config({
     path:"./data/config.env"
 });
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
-    origin:[process.env.FRONTEND_URl],
-    methods:["GET","POST","PUT","DELETE"],
-    credentials:true,
+     origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
  })
 );
 
@@ -28,6 +29,6 @@ app.get("/",(req,res)=>{
     res.send("Hello");
 });
 
-export default app;
+
 
 app.use(errorMiddleware);
